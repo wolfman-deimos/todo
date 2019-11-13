57603ba0b50f293415207aae379b14658cdbc63d
colinmarchbanks <marchbanks.colin@gmail.com>
2019-11-13 09:39:58 -0600

    added api file and js

---
+++ src/index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const express = require('express');
+
+const api = express();
+api.listen(3000, () => {
+  console.log('API up and running!');
+});
+
+api.get('/', (req, res) => {
+  console.log(req);
+  res.send('Hello, world!');
+});
